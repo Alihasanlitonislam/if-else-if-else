4fb9ebf63d102bcc35b7cf838a272b2b21b32174
Alihasanlitonislam <110688357+Alihasanlitonislam@users.noreply.github.com>
2022-09-30 01:42:50 +0600

    git add in fi else-if else

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const number = 90;
 if (number >= 80) {
     console.log("A+        GPA5")
 }
+else if (number >= 70) {
+    console.log("A        4.50")
+}
+else if (number >= 60) {
+    console.log("A-        4.00")
+}
+else if (number >= 50) {
+    console.log("B        3.50")
+}
+else if (number >= 40) {
+    console.log("C        3.00")
+}
+else if (number >= 50) {
+    console.log("D        2.00")
+}
+else {
+    console.log("D        2.00")
+}
